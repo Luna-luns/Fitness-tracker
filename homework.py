56750db4cf7e21fab94dc109a0424e2142609e8d
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-10-16 00:32:50 +0300

    My project

--- homework.py
+++ homework.py
@@ -1,25 +1,41 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
-
+    def __init__(self, training_type: str, duration: float, distance: float, speed: float, calories: float) -> None:
+        self.training_type = training_type
+        self.duration = round(duration, 3)
+        self.distance = round(distance, 3)
+        self.speed = round(speed, 3)
+        self.calories = round(calories, 3)
+    
+    def get_message(self) -> str:
+        return f'Тип тренировки: {self.training_type}; Длительность: {self.duration} ч.; Дистанция: {self.distance} км; Ср. скорость: {self.speed} км/ч; Потрачено ккал: {self.calories}.'
 
 class Training:
     """Базовый класс тренировки."""
+
+    LEN_STEP: float = 0.65
+    M_IN_KM: int = 1000
+    CALORIES_MEAN_SPEED_MULTIPLIER = 18
+    CALORIES_MEAN_SPEED_SHIFT = 1.79 
 
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
-        pass
+        self.action = action
+        self.duration = duration
+        self.weight  = weight
+
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+        return self.action * self.LEN_STEP / self.M_IN_KM 
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        distance = self.get_distance()
+        return distance / self.duration 
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -27,33 +43,59 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+        distance = self.get_distance()
+        speed = self.get_mean_speed()
+        calories = self.get_spent_calories()
+        return InfoMessage(type(self).__name__, self.duration, distance, speed, calories)
 
 
 class Running(Training):
     """Тренировка: бег."""
-    pass
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        mean_spead = self.get_mean_speed()
+        return (self.CALORIES_MEAN_SPEED_MULTIPLIER * mean_spead + self.CALORIES_MEAN_SPEED_SHIFT) * self.weight / self.M_IN_KM * self.duration
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float, height: float) -> None:
+        super().__init__(action, duration, weight)
+        self.height = height
 
+    def get_spent_calories(self) -> float:
+        mean_spead = self.get_mean_speed()
+        a = 0.035
+        power = 2
+        b = 0.029
+
+        return ((a * self.weight + (mean_spead ** power // self.height) * b * self.weight) * self.duration) 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float, length_pool: float, count_pool: float) -> None:
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
 
+    def get_mean_speed(self) -> float:
+        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration 
+
+    def get_spent_calories(self) -> float:
+        mean_spead = self.get_mean_speed()
+        a = 1.1
+        b = 2
+        return (mean_spead + a) * b * self.weight * self.duration 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    pass
-
+    training_dictionary: dict = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
+    return training_dictionary[workout_type](*data)
 
 def main(training: Training) -> None:
     """Главная функция."""
-    pass
-
+    info: InfoMessage = training.show_training_info()
+    print(info.get_message())
 
 if __name__ == '__main__':
     packages = [
